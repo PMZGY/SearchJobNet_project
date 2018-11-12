df6585ef4223e1852a213658d0b78b16646d58a1
ponponmusic <anlyfish9@gmail.com>
2018-11-12 14:51:13 +0800

    initial commit

---
+++ SearchJobNet_project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SearchJobNet_project.Startup))]
+namespace SearchJobNet_project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
