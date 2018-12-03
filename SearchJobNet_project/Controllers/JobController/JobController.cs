dc8f84fc05c4a1678439d432d892dbb575965124
mikouko <mikouko606@gmail.com>
2018-12-03 17:54:38 +0800

    建contorller與view連結

---
+++ SearchJobNet_project/Controllers/JobController/JobController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SearchJobNet_project.Controllers.JobController
+{
+    public class JobController : Controller
+    {
+        // 到JobList頁面
+        public ActionResult toJobListView()
+        {
+            return View("JobListView");
+        }
+
+        //到職缺細項頁面
+        public ActionResult toJobDetailView()
+        {
+            return View("JobDetailView");
+        }
+
+        //到我的最愛頁面
+        public ActionResult toMyFavoriteView()
+        {
+            return View("MyFavoriteView");
+        }
+    }
+}
