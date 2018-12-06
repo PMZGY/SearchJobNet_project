640f1e850cbcbe8770e01cf126896c730356ac89
can <can711147@hotmail.com>
2018-12-07 00:12:25 +0800

    new SearchJobModel,JobModel

---
+++ SearchJobNet_project/Models/SearchJobModel/SearchJobModel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace SearchJobNet_project.Models.SearchJobModel
+{
+    public class SearchJobModel
+    {
+        ///<summary> 公司PK </summary>
+        public string Comp_ID { get; set; }
+
+        ///<summary> 公司名稱 </summary>
+        public string CompName { get; set; }
+
+        ///<summary> 工作地點 </summary>
+        public string CityName { get; set; }
+
+        ///<summary> 職缺Pk </summary>
+        public string Job_ID { get; set; }
+
+        ///<summary> 職缺名稱 </summary>
+        public string Occu_Desc { get; set; }
+
+        ///<summary> 職缺性質 </summary>
+        public string Wk_Type { get; set; }
+
+        ///<summary> 職缺大類別代碼 </summary>
+        public string Cjob_ID { get; set; }
+
+        ///<summary> 職缺大類別名稱 </summary>
+        public string Cjob_Name1 { get; set; }
+
+        ///<summary> 工作經驗 </summary>
+        ///public string Experience { get; set; }
+
+        ///<summary> 工作時間 </summary>
+        ///public string WkTime { get; set; }
+
+        //建立假資料,總共有兩筆資料
+        //public SearchJobModel()
+        //{
+        //}
+    }
+}
