ec02b9135fd7cb7535143b408322909c6863f10b
winsonexe <s102213014@gmail.com>
2018-12-09 17:51:44 +0800

    historymodel historycontroller建置  history experts初步建立

--- SearchJobNet_project/Controllers/HistoryController/HistoryController.cs
+++ SearchJobNet_project/Controllers/HistoryController/HistoryController.cs
@@ -1,17 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using HM = SearchJobNet_project.Models.HistoryModel;
 
 namespace SearchJobNet_project.Controllers.HistoryController
 {
-    public class HistoryController : Controller
-    {
-        // GET: History
-        public ActionResult Index()
+	public class HistoryController
+	{
+        // 建立建構子
+        public HistoryController() { }
+
+        #region 歷史紀錄功能 [生成瀏覽紀錄/瀏覽瀏覽紀錄/瀏覽評論紀錄]
+        
+        // 傳入 歷史model, 執行[生成瀏覽職缺紀錄] 功能
+        // 回傳 成功新增資料與否
+        public string insertHistory(HM.HistoryModel historyModel)
         {
-            return View("HistoryView");
+            string msg = "";
+            HM.History hm = new HM.History();
+            msg = hm.insertHistory(historyModel);
+            return msg;
         }
+        // 傳入 使用者PK,職缺PK 執行 [瀏覽瀏覽職缺紀錄] 功能
+        // 回傳 整筆資料或部分資料(list的HistoryModel型態)
+        public List<HM.HistoryModel> browseHistoryjob(string jobID,string userID)
+        {
+            HM.History hm = new HM.History();
+            List<HM.HistoryModel> hmModel = new List<HM.HistoryModel>();
+            hmModel = hm.browseHistoryjob(jobID,userID);
+            return hmModel;
+        }
+
+        
+        // 傳入 使用者PK 執行 [瀏覽評論紀錄] 功能
+        // 回傳 整筆資料或部分資料(list的HistoryModel型態)
+        public List<HM.HistoryModel> browseHistorycomment(string userID)
+        {
+            HM.History hm = new HM.History();
+            List<HM.HistoryModel> hmModel = new List<HM.HistoryModel>();
+            hmModel = hm.browseHistorycomment(userID);
+            return hmModel;
+        }
+
+
+        #endregion
     }
 }
--- SearchJobNet_project/Models/HistoryModel/HistoryModel.cs
+++ SearchJobNet_project/Models/HistoryModel/HistoryModel.cs
@@ -1,38 +1,56 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace SearchJobNet_project.Models.HistoryModel
 {
-    public class HistoryModel
-    {
-        ///<summary> 使用者PK </summary>
+	public class HistoryModel
+	{
+        
+       
+        ///<summary> 發表者 </summary>
+        [Required]
+        [Display(Name = "發表者")]
         public string User_ID { get; set; }
 
-        ///<summary> 職缺Pk </summary>
+        ///<summary> 職缺 </summary>
+        [Required]
+        [Display(Name = "職缺")]
         public string Job_ID { get; set; }
 
+
         ///<summary> 瀏覽時間 </summary>
+        [Display(Name = "發表時間")]
         public string Time { get; set; }
-        //建立假資料,總共有兩筆資料
-        public HistoryModel()
+
+
+        // 取出假資料,總共有兩筆資料
+        public List<HistoryModel> getAllHistory()
         {
             // 建立list
-            List<HistoryModel> hm = new List<HistoryModel>();
+            List<HistoryModel> result = new List<HistoryModel>();
 
-            // 塞入第一筆假資料
-            HistoryModel hms = new HistoryModel();
-            hms.User_ID = "zhongzhong";
-            hms.Job_ID = "0";
-            hms.Time = "2018/12/04";
-            
-            hm.Add(hms);
-            
-            // 塞入第二筆假資料
-            hms=new HistoryModel();
-            hms.User_ID = "zhongzhong2";
-            hms.Job_ID = "1";
-            hms.Time = "2018/12/04";
+            // 塞入 假資料
+            result.Add(new HistoryModel
+            {
+                
+                Job_ID = "0",
+                User_ID = "ZhongZhong1",
+                Time = "2018/12/02"
+                
 
-            hm.Add(hms);
+            });
+
+            result.Add(new HistoryModel
+            {
+                Job_ID = "1",
+                User_ID = "ZhongZhong2",
+                Time = "2018/12/04"
+
+            });
+
+            // 回傳 假資料
+            return result;
         }
+
     }
 }
