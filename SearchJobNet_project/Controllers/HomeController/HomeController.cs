94c983568f3764592f4f00c04b485068bee02818
mikouko <mikouko606@gmail.com>
2018-12-13 16:48:56 +0800

    前端viewbag

--- SearchJobNet_project/Controllers/HomeController/HomeController.cs
+++ SearchJobNet_project/Controllers/HomeController/HomeController.cs
@@ -14,17 +14,39 @@ namespace SearchJobNet_project.Controllers.HomeController
         {   
             // 搜尋職缺欄位內容種類
             SJM.SearchJob sjm = new SJM.SearchJob();
-            string[] worktype = new string[2];
-            string[] cityname = new string[2];
-            string[] cjobname = new string[4];
-            worktype[0] = "全職";
-            worktype[1] = "兼職";
-            cityname[0] = "新北市";
-            cityname[1] = "台北市";
-            cjobname[0] = "電子/軟體/半導體";
-            cjobname[1] = "批發/零售";
-            cjobname[2] = "金融投顧及保險";
-            cjobname[3] = "大眾傳播";
+            List<SelectListItem> worktype = new List<SelectListItem>();
+            List<SelectListItem> cityname = new List<SelectListItem>();
+            List<SelectListItem> cjobname = new List<SelectListItem>();
+            worktype.Add(new SelectListItem()
+            {
+                Text = "全職",
+                Value = "0"
+            });
+            worktype.Add(new SelectListItem()
+            {
+                Text = "兼職",
+                Value = "1"
+            });
+            cityname.Add(new SelectListItem()
+            {
+                Text = "新北市",
+                Value = "0"
+            });
+            cityname.Add(new SelectListItem()
+            {
+                Text = "台北市",
+                Value = "1"
+            });
+            cjobname.Add(new SelectListItem()
+            {
+                Text = "電子/軟體/半導體",
+                Value = "0"
+            });
+            cjobname.Add(new SelectListItem()
+            {
+                Text = "批發/零售",
+                Value = "1"
+            });
             ViewBag.WorkType = worktype; 
             ViewBag.CityName = cityname;
             ViewBag.Cjob_Name1 = cjobname;
