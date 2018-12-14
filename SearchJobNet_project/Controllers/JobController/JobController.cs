0da590c92e2e961691fc4b02c7c1cfb1a187d47f
can711147 <can711147@hotmail.com>
2018-12-14 18:36:06 +0800

    reverse v.1

--- SearchJobNet_project/Controllers/JobController/JobController.cs
+++ SearchJobNet_project/Controllers/JobController/JobController.cs
@@ -20,10 +20,14 @@ namespace SearchJobNet_project.Controllers.JobController
             //sjModel = sjm.jobList(searchModel);
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
             ViewBag.CityName = searchModel.CityName;
             ViewBag.Wk_Type = searchModel.Wk_Type;
             ViewBag.CompName = searchModel.CompName;
             ViewBag.Cjob_Name1 = searchModel.Cjob_Name1;
+=======
+            ViewBag.SearchJobCol = searchModel;
+>>>>>>> parent of 622b4ca... SearchJob v.5
 =======
             ViewBag.SearchJobCol = searchModel;
 >>>>>>> parent of 622b4ca... SearchJob v.5
