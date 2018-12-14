35d7e74dfcfbc30872e9e4d7dbe4ab820ae0a780
mikouko <mikouko606@gmail.com>
2018-12-14 18:40:29 +0800

    復原

---
+++ SearchJobNet_project/Controllers/JobController/JobController_BACKUP_19332.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SJM = SearchJobNet_project.Models.SearchJobModel;
+using JM = SearchJobNet_project.Models.JobModel;
+
+namespace SearchJobNet_project.Controllers.JobController
+{
+    public class JobController : Controller
+    {
+        // 到JobList頁面
+        // 傳入 搜尋職缺model,執行 [查看職缺清單] 功能
+        // 回傳 職缺清單
+        public ActionResult Index(SJM.SearchJobModel searchModel)
+        {
+            //SJM.SearchJob sjm = new SJM.SearchJob();
+            //List<SJM.SearchJobModel> sjModel = new List<SJM.SearchJobModel>();
+            //sjModel = sjm.jobList(searchModel);
+<<<<<<< HEAD
+            ViewBag.CityName = searchModel.CityName;
+            ViewBag.Wk_Type = searchModel.Wk_Type;
+            ViewBag.CompName = searchModel.CompName;
+            ViewBag.Cjob_Name1 = searchModel.Cjob_Name1;
+=======
+            ViewBag.SearchJobCol = searchModel;
+>>>>>>> parent of 622b4ca... SearchJob v.5
+            return View("JobListView");
+        }
+
+        public ActionResult SendJobListData(SJM.SearchJobModel searchModel)
+        {
+            SJM.SearchJob sjm = new SJM.SearchJob();
+            List<SJM.SearchJobModel> sjModel = new List<SJM.SearchJobModel>();
+            sjModel = sjm.jobList(searchModel);
+
+            return Json(this.Json(sjModel),JsonRequestBehavior.AllowGet);
+        }
+
+        //到職缺細項頁面
+        // 傳入 職缺PK,執行 [查看職缺細項] 功能
+        // 回傳 整筆資料或部分資料(list的JobModel型態)
+        public ActionResult toJobDetailView(int JobID)
+        {
+            SJM.SearchJob sjm = new SJM.SearchJob();
+            List<JM.JobModel> jmModel = new List<JM.JobModel>();
+            jmModel = sjm.jobDetail(JobID);
+            
+            return View("JobDetailView", jmModel);
+        }
+
+        //到我的最愛頁面
+        public ActionResult toMyFavoriteView()
+        {
+            return View("MyFavoriteView");
+        }
+
+
+    }
+}
--- SearchJobNet_project/Models/SearchJobModel/SearchJob.cs
+++ SearchJobNet_project/Models/SearchJobModel/SearchJob.cs
@@ -135,10 +135,10 @@ namespace SearchJobNet_project.Models.SearchJobModel
             #endregion
             // 取出職缺清單
             #region[ 取出職缺清單 ]
-            string SQLComment = (sjm.CompName == null) ? "" : "AND C.COMPNAME LIKE '%"+ sjm.CompName + "%' " ;
+            string SQLComment = (sjm.CompName == null) ? "" : "AND C.COMPNAME =" + sjm.CompName;
             string SQLComment1 = (sjm.Cjob_Name1 == "不拘") ? "" : "AND JT.CJOB_NAME1 = " + sjm.Cjob_Name1;
             string SQLComment2 = (sjm.CityName == "不拘") ? "" : " AND J.CITYNAME = " + sjm.CityName;
-            string SQLComment3 = (sjm.Wk_Type == "不拘") ? "" : "AND J.WK_TYPE = " + sjm.Wk_Type;
+            string SQLComment3 = (sjm.Wk_Type == "不拘") ? "" : "AND J.WK_TYPE =" + sjm.Wk_Type;
             DataTable dt = bsc.ReadDB(
                             string.Format(
                             @"SELECT J.COMP_ID,C.COMPNAME,J.CITYNAME,J.JOB_ID,J.OCCU_DESC,J.WK_TYPE,J.CJOB_ID,JT.CJOB_NAME1
@@ -147,7 +147,7 @@ namespace SearchJobNet_project.Models.SearchJobModel
                                   AND J.CJOB_ID = JT.CJOB_ID
                                   AND C.COMP_ID = J.COMP_ID
                                   {0} {1} {2} {3}"
-                                  , SQLComment, SQLComment1, SQLComment2, SQLComment3)
+                                  , SQLComment1, SQLComment2, SQLComment3, SQLComment)
                                 );
             // 將DataTable的資料轉換為model
             List<SJM.SearchJobModel> joblist = new List<SJM.SearchJobModel>();
@@ -173,7 +173,7 @@ namespace SearchJobNet_project.Models.SearchJobModel
 
 
         // 搜尋職缺細項
-        public JM.JobModel jobDetail(int jobID)
+        public List<JM.JobModel> jobDetail(int jobID)
         {
             
             // SQL指令 撈出職缺細項
@@ -197,27 +197,29 @@ namespace SearchJobNet_project.Models.SearchJobModel
                                   AND J.CJOB_ID = JT.CJOB_ID"
                                   , jobID)
                                 );
-            JM.JobModel jmModel = new JM.JobModel();
+            List<JM.JobModel> jmModel = new List<JM.JobModel>();
             // 將DataTable的資料轉換為model 將職缺細項列出
-
-            jmModel.Job_ID = Convert.ToInt16(dt.Rows[0][0]);
-            jmModel.Occu_Desc = dt.Rows[0][1].ToString();
-            jmModel.Wk_Type = dt.Rows[0][2].ToString();
-            jmModel.Cjob_ID = Convert.ToInt16(dt.Rows[0][3]);
-            jmModel.Cjob_Name1 = dt.Rows[0][4].ToString();
-            jmModel.AvailReqNum = Convert.ToInt16(dt.Rows[0][5]);
-            jmModel.Stop_Date = dt.Rows[0][6].ToString();
-            jmModel.Job_Detail = dt.Rows[0][7].ToString();
-            jmModel.CityName = dt.Rows[0][8].ToString();
-            jmModel.Experience = dt.Rows[0][9].ToString();
-            jmModel.WkTime = dt.Rows[0][10].ToString();
-            jmModel.SalaryCd = dt.Rows[0][11].ToString();
-            jmModel.EdGrDesc = dt.Rows[0][12].ToString();
-            jmModel.Url_Query = dt.Rows[0][13].ToString();
-            jmModel.Comp_ID = Convert.ToInt16(dt.Rows[0][14]);
-            jmModel.CompName = dt.Rows[0][15].ToString();
-            jmModel.TranDate = dt.Rows[0][16].ToString();
-              
+            jmModel.Add(new JM.JobModel
+            {
+                Job_ID = Convert.ToInt16(dt.Rows[0][0]),
+                Occu_Desc = dt.Rows[0][1].ToString(),
+                Wk_Type = dt.Rows[0][2].ToString(),
+                Cjob_ID = Convert.ToInt16(dt.Rows[0][3]),
+                Cjob_Name1 = dt.Rows[0][4].ToString(),
+                AvailReqNum = Convert.ToInt16(dt.Rows[0][5]),
+                Stop_Date = dt.Rows[0][6].ToString(),
+                Job_Detail = dt.Rows[0][7].ToString(),
+                CityName = dt.Rows[0][8].ToString(),
+                Experience = dt.Rows[0][9].ToString(),
+                WkTime = dt.Rows[0][10].ToString(),
+                SalaryCd = dt.Rows[0][11].ToString(),
+                EdGrDesc = dt.Rows[0][12].ToString(),
+                Url_Query = dt.Rows[0][13].ToString(),
+                Comp_ID = Convert.ToInt16(dt.Rows[0][14]),
+                CompName = dt.Rows[0][15].ToString(),
+                TranDate = dt.Rows[0][16].ToString()
+                        
+            });
                 
 
                 #endregion
