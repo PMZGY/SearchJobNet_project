a5784cdf8108329ecbb616b8de182d17ce7700de
andy tsai <yuanting_tsai1992@gmail.com>
2018-12-16 14:15:03 +0800

    Revert "Revert "jobdetail.js for debugged""
    This reverts commit 320763fdea5597f73dece94b66481d0c48f120ec.

--- SearchJobNet_project/scripts/JobDetail.js
+++ SearchJobNet_project/scripts/JobDetail.js
@@ -4,24 +4,25 @@
         "grid-gap": "16px",
         "grid-template-columns": "repeat(2, 1fr) minmax(50px, 1fr)",
         "grid-template-rows": "repeat(10, min-content)"
-    })
+    });
+
     $("#dtable").css({
         "align": "center",
         "width": "300px",
         "border": "10px"
-    })
+    });
 
     $(".column").css({
         "float": "left",
         "width": "100%",
         "padding": "5px",
-    })
+    });
 
     $(".row:after").css({
         "content": "",
         "display": "table",
         "clear": "both"
-    })
+    });
 
     /*function gridView() {
         for (i = 0; i < elements.length; i++) {
@@ -37,62 +38,81 @@
         "border": "10px #2E5C6E solid",
         "margin-bottom": "50px"
     });
-
     /*
     array.each(function () {
         $("#" + key).innerHTML(value)
     })*/
 
     /* 以下function */
-    
-    $("Document").ready(function (jobID) {
-        console.log(jobID);
-            
-            /*用jobID去撈comment*/
-           var action = 'Comment/browseMenberComment'   
 
-           var formData = {
-               Job_ID: jobID,
-               phase: 1
+
+    $("Document").ready(function (abc) {
+        console.log(jobID);
+
+            /*用jobID去撈comment*/
+           var action = '../Comment/browseMemberComment'
+
+           jobID = 1;
+           var JID = Convert.Int32(jobID); 
+
+          var formData = {
+          //     ID   : JID,
+              ID : jobID,
+              phase: 1
            };
 
-           $.post(action,formData)
+           console.log('post???');
+
+        $.post(action,formData)
                 .done(function (Data) {
+
+                    console.log('posted');
                     console.log(Data);
-                    /*傳一個List<CommentModel> 進來  each 建一個 container 來放資料*/
-                    $.each(Data,function (index , Model) {
-                        //建一個model來接值
-                        var commentModel = Model
+                    console.log('posted2');
+
+                    var arr = Data;
+
+                    $.each(arr, function (key, value) {
+                        console.log(key);
+                        alert(key.val() + ": " + value.val());
+                    });
+
+                    /*傳一個Data: List<CommentModel> 進來  each 建一個 container 來放資料*/
+                    //$.forEach(function (Model) {
+
+                    //    console.log(Model);
+                    //    //建一個model來接值
+                    //    var commentModel = Model;
                  
-                        // var commentModel
+                    //    // var commentModel
 
-                        var comment_container = '<div class="container">' +
-                                                    '<div class="row">' +
-                                                        '<div class="column">' +
-                                                            commentModel.Job_ID
-                                                        '</div>' +
-                                                    '</div>' +
-                                                    '<div class="row">' +
-                                                        '<div class="column">' +
-                                                            //commentModel.userid
-                                                        '</div>' +
-                                                        '<div class="column">' +
-                                                            //commentModel.time
-                                                        '</div>' +
-                                                    '</div>' +
-                                                    '<div class="row">' +
-                                                        '<div class="column">' +
-                                                            //commentModel.contentTEXT
-                                                        '</div>' +
-                                                    '</div>' +
-                                                '</div>'
+                    //    var comment_container = '<div class="container">' +
+                    //                                '<div class="row">' +
+                    //                                    '<div class="column">' +
+                    //                                        commentModel.Job_ID +
+                    //                                    '</div>' +
+                    //                                '</div>' +
+                    //                                '<div class="row">' +
+                    //                                    '<div class="column">' +
+                    //                                        //commentModel.userid
+                    //                                    '</div>' +
+                    //                                    '<div class="column">' +
+                    //                                        //commentModel.time
+                    //                                    '</div>' +
+                    //                                '</div>' +
+                    //                                '<div class="row">' +
+                    //                                    '<div class="column">' +
+                    //                                        //commentModel.contentTEXT
+                    //                                    '</div>' +
+                    //                                '</div>' +
+                    //                            '</div>';
 
-                        $(".comment_table").append(comment_container)
-                    })
+                    //    $(".comment_table").append(comment_container);
+                    //})
                 })
             .fail(function () {
                 $(".comment_table").append('<div style="color:#AAA; text-align:center;">沒有評論...</div>')
             });
-        /*  */
+        /* comment end */
        })   
 })
